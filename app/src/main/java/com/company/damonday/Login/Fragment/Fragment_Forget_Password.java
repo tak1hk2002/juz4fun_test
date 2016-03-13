b5017643a8408433f376cb645dbcc425c754f459
tak1hk2002 <tak1hk2002@gmail.com>
2016-03-13 19:06:51 +0800

    alan_13/03/2016_19:05

--- app/src/main/java/com/company/damonday/Login/Fragment/Fragment_Forget_Password.java
+++ app/src/main/java/com/company/damonday/Login/Fragment/Fragment_Forget_Password.java
@@ -35,7 +35,7 @@ public class Fragment_Forget_Password extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
+     * @param param1 Parameter 1. 
      * @param param2 Parameter 2.
      * @return A new instance of fragment Fragment_Forget_Password.
      */
